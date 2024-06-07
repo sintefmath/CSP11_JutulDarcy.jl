f16c2229bfe4018848667fbd7cb38553ba38b438
Jacques Franc <49998870+jafranc@users.noreply.github.com>
2024-06-07 20:51:40 +0200

    Create docker_run_mrst_grid_spe11.jl

---
+++ docker_run_mrst_grid_spe11.jl
@@ -0,0 +1,26 @@
+using Jutul, JutulDarcy, HYPRE, CSP11
+
+basename = "cPEBI_2640x380"
+basename = "cPEBI_819x117"
+basename = "horizon-cut_100x50"
+
+case, name = setup_spe11_caseb_from_mrst_grid(basename, thermal = true);
+pth = jutul_output_path(name, subfolder = "csp11")
+domain = reservoir_domain(case.model)
+
+hook, csv_pth, f = CSP11.get_reporting_hook(pth, domain)
+
+res = simulate_reservoir(case,
+    output_path = pth,
+    max_timestep = 1*si_unit(:year),
+    report_level = 0,
+    tol_cnv = 0.001,
+    post_ministep_hook = hook,
+    relaxation = SimpleRelaxation(),
+    max_nonlinear_iterations = 20,
+    max_timestep_cuts = 100,
+    info_level = 1
+)
+ws, states = res;
+
+  

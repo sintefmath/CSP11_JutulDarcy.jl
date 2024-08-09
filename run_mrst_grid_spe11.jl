a8ca5b55014d750994aca355a98030d42f37dbef
Olav Møyner <olav.moyner@gmail.com>
2024-08-09 14:39:42 +0200

    Split script into simulator setup and simulation

--- run_mrst_grid_spe11.jl
+++ run_mrst_grid_spe11.jl
@@ -2,18 +2,19 @@ using Jutul, JutulDarcy, HYPRE, CSP11, GLMakie
 
 # basename = "cPEBI_2640x380"
 # basename = "cPEBI_819x117"
-# basename = "horizon-cut_100x50";specase = :b
-# basename = "struct20x20x20";specase = :c
-basename = "struct100x100x100";specase = :c
+basename = "horizon-cut_100x50"; specase = :b
+basename = "struct20x20x20"; specase = :c
+# basename = "struct50x50x50"; specase = :c
+
+# basename = "struct100x100x100"; specase = :c
 
 case, name = setup_spe11_case_from_mrst_grid(basename, case = specase, thermal = true);
 pth = jutul_output_path(name, subfolder = "csp11")
 domain = reservoir_domain(case.model)
 
 hook, csv_pth, f = CSP11.get_reporting_hook(pth, domain);
-
-restart = true #'true' will continue sim where it left off
-res = simulate_reservoir(case,
+##
+sim, config = setup_reservoir_simulator(case,
     output_path = pth,
     max_timestep = 1*si_unit(:year),
     report_level = 1,
@@ -22,11 +23,15 @@ res = simulate_reservoir(case,
     relaxation = SimpleRelaxation(),
     max_nonlinear_iterations = 20,
     max_timestep_cuts = 100,
-    info_level = 1,
-    restart = restart
+);
+##
+restart = false #'true' will continue sim where it left off
+res = simulate_reservoir(case,
+    simulator = sim,
+    config = config,
+    restart = restart,
 );
 ws, states = res;
-
 ##write to mrst format
 mrst_output_path = pth*"_mrst"
 JutulDarcy.write_reservoir_simulator_output_to_mrst(case.model[:Reservoir], states, res.result.reports, case.forces, mrst_output_path)

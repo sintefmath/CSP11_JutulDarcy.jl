3b846bd333ccc2d159bca0e73fbf599ec83098f2
Olav Møyner <olav.moyner@gmail.com>
2024-08-30 09:27:25 +0200

    Update run_cases_for_delivery.jl

--- run_cases_for_delivery.jl
+++ run_cases_for_delivery.jl
@@ -12,8 +12,11 @@ cases_c = [
     "c_170x100x50"
 ]
 
+allcases = [cases_b, cases_c]
+allcases = [["b_400x60"]]
+
 kgrad_to_run = [:tpfa, :avgmpfa]
-for cases_to_run in [cases_b, cases_c]
+for cases_to_run in allcases
     for kg in kgrad_to_run
         for basename in cases_to_run
             specase = Symbol(basename[1])
@@ -26,16 +29,19 @@ for cases_to_run in [cases_b, cases_c]
             domain = reservoir_domain(case.model)
             nc = number_of_cells(domain)
             println("Running case $name with $nc cells")
-            pth = jutul_output_path(name, subfolder = "csp11_delivery")
-            hook, csv_pth, f = CSP11.get_reporting_hook(pth, domain)
+            pth = jutul_output_path(name, subfolder = "csp11_delivery_aug30")
+            hook, csv_pth, f = CSP11.get_reporting_hook(pth, domain, specase = specase)
             simulate_reservoir(case,
                 output_path = pth,
                 max_timestep = 1*si_unit(:year),
-                report_level = 1,
+                report_level = 0,
+                tol_cnv = 0.01,
                 post_ministep_hook = hook,
                 relaxation = SimpleRelaxation(),
                 max_nonlinear_iterations = 20,
                 max_timestep_cuts = 100,
+                output_states = false,
+                output_reports = false,
                 info_level = 1
             );
         end

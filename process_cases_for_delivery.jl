540e73ff509fd53d7dd49eb1c7bfd34f8fad0757
Olav Møyner <olav.moyner@gmail.com>
2024-08-30 15:33:44 +0200

    Update process_cases_for_delivery.jl

--- process_cases_for_delivery.jl
+++ process_cases_for_delivery.jl
@@ -1,10 +1,16 @@
 cases_to_run = [
-    "c_10x10x10"
+    "nudge_b_400x60",
+    "nudge_b_819x117",
+    "nudge_b_2640x380",
+    "nudge_c_10x10x10",
+    "nudge_c_85x50x40",
+    "nudge_c_170x100x50",
+    "nudge_c_170x100x100"
 ]
 kgrad_to_run = [:tpfa, :avgmpfa]
 for kg in kgrad_to_run
     for basename in cases_to_run
-        specase = Symbol(basename[1])
+        specase = Symbol(basename[7])
         case, name = setup_spe11_case_from_mrst_grid(basename,
             case = specase,
             thermal = true,

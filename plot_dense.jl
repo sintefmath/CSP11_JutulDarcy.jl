811cc9ebe1eea13b5a4653624374e2a8fa8a0edd
Olav Møyner <olav.moyner@gmail.com>
2024-09-01 09:42:40 +0200

    Update plot_dense.jl

--- plot_dense.jl
+++ plot_dense.jl
@@ -9,9 +9,52 @@ function addcase!(pth, name = "Case $(length(paths)+1)")
     push!(pnames, name)
 end
 
-specase = :b
-addcase!(joinpath(@__DIR__, "data/compare/b/spe11b_spatial_map_1000y.csv"))
-addcase!(joinpath(@__DIR__, "data/compare/generated/spe11b_spatial_map_1000y.csv"))
+if false
+    specase = :b
+    # addcase!(joinpath(@__DIR__, "data/compare/b/spe11b_spatial_map_1000y.csv"))
+    # addcase!(joinpath(@__DIR__, "data/compare/generated/spe11b_spatial_map_1000y.csv"))
+
+    addcase!(
+        raw"D:\jobb\spe11_delivery\aug31\delivery_sintef\delivery\spe11b\result1\spe11b_spatial_map_1000y.csv",
+        "TPFA 819x117"
+    )
+
+    addcase!(
+        raw"D:\jobb\spe11_delivery\aug31\delivery_sintef\delivery\spe11b\result2\spe11b_spatial_map_1000y.csv",
+        "MPFA 819x117"
+    )
+
+    
+    addcase!(
+        raw"D:\jobb\spe11_delivery\aug31\delivery_sintef\delivery\spe11b\result3\spe11b_spatial_map_1000y.csv",
+        "TPFA 400x60"
+    )
+
+    addcase!(
+        raw"D:\jobb\spe11_delivery\aug31\delivery_sintef\delivery\spe11b\result4\spe11b_spatial_map_1000y.csv",
+        "MPFA 400x60"
+    )
+
+
+else
+    specase = :c
+    # addcase!(joinpath(@__DIR__, "data/compare/generated/spe11c_spatial_map_1000y.csv"))
+
+    addcase!(
+        raw"D:\jobb\spe11_delivery\aug31\delivery_sintef\delivery\spe11c\result1\spe11c_spatial_map_1000y.csv",
+        "TPFA 170x100x50"
+    )
+
+    addcase!(
+        raw"D:\jobb\spe11_delivery\aug31\delivery_sintef\delivery\spe11c\result2\spe11c_spatial_map_1000y.csv",
+        "MPFA 170x100x50"
+    )
+
+    
+    addcase!(
+        raw"D:\jobb\spe11_delivery\aug31\delivery_sintef\delivery\spe11c\result3\spe11c_spatial_map_1000y.csv",
+        "TPFA 170x100100"
+    )
 
 # specase = :c
 # pth = "/media/moyner/Data/jutul_output/csp11_delivery/spe11c_c_10x10x10_thermal_cv_tpfa/dense/spe11b_spatial_map_1000y.csv"
@@ -33,22 +76,46 @@ else
     dims = [168, 100, 120]
 end
 
-function get_data(pth)
+function get_data(en)
+    i, pth = en
     df = CSV.read(pth, DataFrame)
     ##
     f = " total mass CO2 [kg]"
     f = " total mass CO2 [kg]"
-    f = " phase mass density water [kg/m3]"
+    # f = " phase mass density water [kg/m3]"
     # f = "# x [m]"
-    f = " gas saturation [-]"
+    # f = " # y [m]"
+    # if i < 4
+    #     f = " y [m]"
+    # else
+    #     f = " z [m]"
+    # end
+    # f = " gas saturation [-]"
     # f = 
+    x = df[:, "# x [m]"]
+    y = df[:, " y [m]"]
+    z  = df[:, " z [m]"]
+
+    Lx = maximum(x)
+    Ly = maximum(y)
+    Lz = maximum(z)
+    
     val = df[:, f]
-    val = reshape(val, dims...)
+    function sortfunction(i)
+        xi = x[i]
+        yi = y[i]
+        zi = z[i]
+
+        return zi*(Lx*Ly) + yi*Lx + xi
+    end
+    ix = sort(eachindex(val), by = sortfunction)
+
+    val = reshape(val[ix], dims...)
     @info "Sum" sum(val)
     return val
 end
 
-data = map(get_data, paths);
+data = map(get_data, enumerate(paths));
 ##
 ndata = length(data)
 fig = Figure(size = (1200, 1000))
@@ -59,12 +126,14 @@ begin
     for (i, val) in enumerate(data)
         if specase == :b
             f! = heatmap!
+            A = Axis
         else
             f! = volume!
+            A = Axis3
         end
         cmin = min(minimum(val), cmin)
         cmax = max(maximum(val), cmax)
-        ax = Axis(fig[1, i], title = pnames[i])
+        ax = A(fig[1, i], title = pnames[i])
         f!(ax, val)
     end
 end

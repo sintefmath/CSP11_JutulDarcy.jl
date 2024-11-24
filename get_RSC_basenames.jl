b991e6ac326b848ff50f2ad5f66aa53cd1d4eebe
Kristian Holme <holme.kristian@gmail.com>
2024-11-24 20:46:31 +0100

    add mrst gridcase names, for loading mrst data in julia

--- get_RSC_basenames.jl
+++ get_RSC_basenames.jl
@@ -33,19 +33,45 @@ function get_RSC_basenames(;grids = [:C, :HC, :CC, :PEBI, :QT, :T],
     end
 
     allcases = []
+    gridcases = []  # For MATLAB-style grid names
+    
     for specs in resolutionSpecs
         res = specs[1]
         refQT = specs[2]
         refT = specs[3]
+        
+        # Parse nx and nz from resolution string (e.g., "140x75")
+        nx, nz = parse.(Int, split(res, "x"))
+        
+        # Parse QT and T values from their reference strings
+        qt = parse(Float64, replace(refQT, "_" => "."))
+        t = parse(Float64, replace(refT, "_" => "."))
+        
         for grid in cartGrids
             push!(allcases, grid*"_"*res)
+            
+            # Add corresponding MATLAB grid name based on grid type
+            if occursin("b_C_", grid)
+                push!(gridcases, "struct$(nx)x$(nz)")
+            elseif occursin("b_HC_", grid)
+                push!(gridcases, "horz_ndg_cut_PG_$(nx)x$(nz)")
+            elseif occursin("b_CC_", grid)
+                push!(gridcases, "cart_ndg_cut_PG_$(nx)x$(nz)")
+            elseif occursin("b_PEBI_", grid)
+                push!(gridcases, "cPEBI_$(nx)x$(nz)")
+            end
         end
+        
         for grid in QTGrids
             push!(allcases, grid*refQT)
+            push!(gridcases, "gq_pb$qt")
         end
+        
         for grid in TGrids
             push!(allcases, grid*refT)
+            push!(gridcases, "5tetRef$t")
         end
     end
-    return allcases
+    
+    return allcases, gridcases
 end
